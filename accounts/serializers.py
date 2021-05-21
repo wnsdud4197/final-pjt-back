e9d466fcf5dc62f71a71c7ede56f47dd7a1fa460
eunzi-kim <rladmswlek@naver.com>
2021-05-21 15:08:27 +0900

    Add signup feature

---
+++ accounts/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+
+    image = serializers.ImageField(use_url=True)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'image')
+        extra_kwargs = {'password': {'write_only': True}}
