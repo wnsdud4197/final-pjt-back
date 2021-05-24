2984e5516453e236c8e6d1f08ec39fbf65729e62
eunzi-kim <rladmswlek@naver.com>
2021-05-24 11:28:44 +0900

    Add random feature

--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.movie_list),
     path('genres/', views.genre_list),
     path('languages/', views.language_list),
+    path('movierandom/', views.movie_random),
 ]
